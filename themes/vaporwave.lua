82c189ea2127981e67dcb6f3a086ede3261f24c6
BeefReal <164821791+BeefReal@users.noreply.github.com>
2025-06-07 20:59:25 -0400

    Update vaporwave.lua

---
+++ themes/vaporwave.lua
@@ -0,0 +1,7 @@
+return {
+    BackgroundColor = Color3.fromRGB(50, 10, 60),
+    AccentColor = Color3.fromRGB(200, 100, 255),
+    TextColor = Color3.fromRGB(255, 240, 255),
+    ButtonColor = Color3.fromRGB(150, 30, 200),
+    ScrollbarColor = Color3.fromRGB(255, 130, 255),
+}
