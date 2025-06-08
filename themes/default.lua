9f2c77588c26a82044e4a0b98bd0f5edc43ac452
BeefReal <164821791+BeefReal@users.noreply.github.com>
2025-06-07 20:59:10 -0400

    Update default.lua

---
+++ themes/default.lua
@@ -0,0 +1,7 @@
+return {
+    BackgroundColor = Color3.fromRGB(20, 20, 25),
+    AccentColor = Color3.fromRGB(100, 100, 120),
+    TextColor = Color3.fromRGB(255, 255, 255),
+    ButtonColor = Color3.fromRGB(60, 60, 70),
+    ScrollbarColor = Color3.fromRGB(120, 120, 255),
+}
