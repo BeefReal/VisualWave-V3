68d904214ae28177bd65899c772307e90d0c6afe
BeefReal <164821791+BeefReal@users.noreply.github.com>
2025-06-07 20:59:38 -0400

    Update dark.lua

---
+++ themes/dark.lua
@@ -0,0 +1,7 @@
+return {
+    BackgroundColor = Color3.fromRGB(10, 10, 10),
+    AccentColor = Color3.fromRGB(50, 50, 50),
+    TextColor = Color3.fromRGB(220, 220, 220),
+    ButtonColor = Color3.fromRGB(40, 40, 40),
+    ScrollbarColor = Color3.fromRGB(80, 80, 80),
+}
